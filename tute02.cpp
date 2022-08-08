12732e34b9a3f356742b600f60b66ec9111bc692
Navodya Wickramasuriya <110513344+IT21292590@users.noreply.github.com>
2022-08-08 07:18:15 +0000

    modified

--- tute02.cpp
+++ tute02.cpp
@@ -42,3 +42,40 @@ int main()
   
    return 0;
 }
+
+
+//c++ program
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   double salary, netSalary;
+   int etype, otHrs, otRate;
+  
+   cout << "Enter Employee Type : " ;
+   cin >> etype ;
+   cout << "Enter Salary  : ";
+   cin >> salary;
+   cout << "Enter OtHrs : ";
+   cin >> otHrs;
+   
+   switch (etype) {
+      case 1 :
+          otRate = 1000;
+          break;
+      case 2 :
+          otRate = 1500;
+          break;
+      default :
+          otRate = 1700;
+          break;
+   }
+
+
+   netSalary = salary + otHrs* otRate;
+   cout << "Net Salary is  : "<< netSalary;
+  
+   return 0;
+}
